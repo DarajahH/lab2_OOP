37f533718471f81a088b1f16e291844c4d8a240a
Darajah <darajah@gmail.com>
2025-02-10 23:58:35 -0500

    good

--- src/main/java/academy/javapro/Tesla.java
+++ src/main/java/academy/javapro/Tesla.java
@@ -14,27 +14,19 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
     	
     }
 
-    public void startCar() {
-        System.out.println("Tesla is now powered on silently.");
-        isRunning = true;
-    }
-    
-    @Override
-    public void stopEngine() {
-        System.out.println("Tesla is shutting down.");
-        isRunning = false;
-    }
-    
+ 
 	@Override
 	public void enableAutopilot() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Autopilot engaged.");
+        autopilotEnabled = true;
 	}
 
 	@Override
 	public void disableAutopilot() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Autopilot disengaged.");
+        autopilotEnabled = false;
 	}
 
 	@Override
@@ -46,31 +38,39 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
 	@Override
 	public void startEngine() {
 		// TODO Auto-generated method stub
+		 System.out.println("Tesla is now powered on...");
+	        isRunning = true;
 		
 	}
 
 	@Override
 	public void stopEngine() {
 		// TODO Auto-generated method stub
-		
+		 System.out.println("Tesla is shutting down...");
+	        isRunning = false;
 	}
 
 	@Override
 	public void accelerate() {
 		// TODO Auto-generated method stub
-		
+		if (isRunning) {
+            System.out.println("Tesla is accelerating..");
+        } else {
+            System.out.println("Start Engine");
+        }
 	}
 
 	@Override
 	public void brake() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Tesla is braking...");
 	}
 
 	@Override
 	public void Charge() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Tesla is now charging...");
+        charging = true;
 	}
 
 	@Override
@@ -79,10 +79,18 @@ public class Tesla extends Vehicle implements Electric, Autonomous {
 		return false;
 	}
     
-    
-    
     // TODO: Initialize autopilotEnabled and charging to false
 
     // TODO: Implement all required methods from Vehicle, Electric, and Autonomous
     // Each method should include appropriate print statements
+	
+	// Getters and Setters
+
+    public void setAutopilotEnabled(boolean autopilotEnabled) {
+        this.autopilotEnabled = autopilotEnabled;
+    }
+
+    public void setCharging(boolean charging) {
+        this.charging = charging;
+    }
 }
